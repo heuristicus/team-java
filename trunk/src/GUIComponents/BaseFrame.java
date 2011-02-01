4f431088b1fcb85897cb5cc753972734e075be9a
heuristicus <m.staniaszek@gmail.com>
2011-02-01 17:00:10 +0000

    Moved stuff around. Makes sense to have the GUI components like frames, panels etc in the same package, hence GUIComponents.
    git-svn-id: https://codex.cs.bham.ac.uk/svn/teamjava/B3@36 88c3a9b8-8305-4942-bc19-8954e2f916ea

---
+++ trunk/src/GUIComponents/BaseFrame.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUIComponents;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author michal
+ */
+public class BaseFrame {
+
+    JFrame bFrame;
+    int width, height;
+
+    public BaseFrame(int width, int height) {
+        this.width = width;
+        this.height = height;
+        initFrame();
+    }
+
+    /*
+     * Initialises the frame. Sets the default close operation to exit.
+     * Sets the width and height of the frame to the value passed in to
+     * the constructor. Sets the frame to be enabled and sets it to be visible.
+     */
+    private void initFrame() {
+        bFrame = new JFrame("ShootyThing");
+        bFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        bFrame.setSize(width, height);
+        bFrame.setEnabled(true);
+        bFrame.setVisible(true);
+    }
+
+    /**
+     * Adds a panel to the frame.
+     * @param addPanel Panel to add to the frame.
+     */
+    public void addPanel(JPanel addPanel) {
+        bFrame.add(addPanel);
+    }
+}
