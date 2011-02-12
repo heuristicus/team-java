1c6bdb3b527c88279ae2195acc3f45bd721076ac
jxk988 <jxk988@cs.bham.ac.uk>
2011-02-12 13:38:04 +0000

    Made very basic functionality for the weapon and projectile classes.
    git-svn-id: https://codex.cs.bham.ac.uk/svn/teamjava/B3@57 88c3a9b8-8305-4942-bc19-8954e2f916ea

--- trunk/src/Weapon/BasicWeapon.java
+++ trunk/src/Weapon/BasicWeapon.java
@@ -1,14 +1,26 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package Weapon;
+
+import Projectile.BasicProjectile;
+import java.awt.Graphics2D;
 
 /**
  *
- * @author michal
+ * @author Jere
  */
 public class BasicWeapon extends Weapon {
+
+    public BasicWeapon()
+    {
+        super(new BasicProjectile(), 20);
+    }
+
+    @Override
+    public void fire(Graphics2D g, int x, int y) {
+        for(int i = 0; i < super.range*10;i++)
+        {
+            projectile.move(x,y-i);
+            projectile.draw(g);
+        }
+    }
 
 }
--- trunk/src/Weapon/Weapon.java
+++ trunk/src/Weapon/Weapon.java
@@ -1,14 +1,29 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package Weapon;
+
+import Projectile.Projectile;
+import java.awt.Graphics2D;
 
 /**
  *
- * @author michal
+ * @author Jere
  */
 public abstract class Weapon {
+
+    // range = pixels*10. So 1 range is 10 pixels moved up.
+    protected int range;
+    protected Projectile projectile;
+
+    public Weapon(Projectile projectile, int range)
+    {
+        this.projectile = projectile;
+        this.range = range;
+    }
+
+    // Accessory methods
+    public int getRange() { return range; }
+    public int getDamage() { return projectile.getDamage(); }
+
+    // Abstract methods.
+    public abstract void fire(Graphics2D g, int x, int y);
 
 }
