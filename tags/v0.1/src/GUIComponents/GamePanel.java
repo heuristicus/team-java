ca3d2883f87abe25d0f07d59445714928f9d6682
heuristicus <m.staniaszek@gmail.com>
2011-02-15 11:23:19 +0000

    Did things properly.
    git-svn-id: https://codex.cs.bham.ac.uk/svn/teamjava/B3@73 88c3a9b8-8305-4942-bc19-8954e2f916ea

--- tags/v0.1/src/GUIComponents/GamePanel.java
+++ tags/v0.1/src/GUIComponents/GamePanel.java
@@ -85,7 +85,7 @@ public class GamePanel extends JPanel {
         // Polygon shipone = one.ship(g2);
         movement();
         g2.setColor(Color.green);
-        one.setLocation(player1_x, player1_y);
+        one.setLocation(new Point(player1_x, player1_y));
         mouse = a.isMouse();
         shootGame.addUnitToArray(one);
         try {
