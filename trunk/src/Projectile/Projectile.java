b901b1f1725ac01f436456da201bc42b8d168888
jxk988 <jxk988@cs.bham.ac.uk>
2011-02-15 14:50:47 +0000

    Small modification to the projectile class.
    git-svn-id: https://codex.cs.bham.ac.uk/svn/teamjava/B3@78 88c3a9b8-8305-4942-bc19-8954e2f916ea

--- trunk/src/Projectile/Projectile.java
+++ trunk/src/Projectile/Projectile.java
@@ -39,6 +39,8 @@ public abstract class Projectile {
     }
     public void setX(int x) { this.x = x; }
     public void setY(int y) { this.y = y; }
+    public void setX(int x) { this.x = x; }
+    public void setY(int y) { this.y = y; }
 
     public boolean isEnemy() {
         return isEnemy;
