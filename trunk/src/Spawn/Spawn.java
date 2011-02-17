d6e4795335c92350566d43d1bd42d807f2efaf64
dwc928 <dwc928@cs.bham.ac.uk>
2011-02-17 14:43:27 +0000

    Basic Spawn class creates an enemy at a random point (1 of 2).
    git-svn-id: https://codex.cs.bham.ac.uk/svn/teamjava/B3@94 88c3a9b8-8305-4942-bc19-8954e2f916ea

--- trunk/src/Spawn/Spawn.java
+++ trunk/src/Spawn/Spawn.java
@@ -5,10 +5,38 @@
 
 package Spawn;
 
+import Weapon.*;
+import Unit.*;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Random;
+
 /**
  *
- * @author michal
+ * @author danielcecil
  */
 public class Spawn {
+
+    private static Enemy spawn1;
+    private static Enemy spawn2;
+    Graphics g;
+    
+    public Spawn(){
+        spawn1 = new Enemy(100, 100, new BasicWeapon(), 100, 200, 50, Color.GREEN);
+        spawn2 = new Enemy(100, 100, new BasicWeapon(), 100, 600, 50, Color.GREEN);
+
+    }
+
+    public Enemy newSpawn(){
+        Enemy en;
+        Random r = new Random();
+        int rand = r.nextInt(2);
+        if(rand == 0){
+            en = spawn1;
+        } else {
+            en = spawn2;
+        }
+        return en;
+    }
 
 }
