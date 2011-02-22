19eaf5e31fdd25f743a8622923c7fbf3c74a442a
heuristicus <m.staniaszek@gmail.com>
2011-02-22 15:35:37 +0000

    Log for previous commit: Sorted out some stuff with drawing units and projectiles. These calculations should be done inside the gamepanel rather than the game class, since drawing doesn't really have anything to do with the game logic.
    git-svn-id: https://codex.cs.bham.ac.uk/svn/teamjava/B3@114 88c3a9b8-8305-4942-bc19-8954e2f916ea

--- trunk/src/Game/Game.java
+++ trunk/src/Game/Game.java
@@ -8,7 +8,6 @@ import Projectile.Projectile;
 import Spawn.Spawn;
 import Unit.Unit;
 import java.awt.Dimension;
-import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
